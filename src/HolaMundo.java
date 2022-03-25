71782e6679b14e9f7b694d7e6407fb5c36d07067
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-03-24 21:55:11 -0600

    26 Ejercicio - Operadores de Asignación en Java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,28 +2,28 @@
 public class HolaMundo {
 
     public static void main(String args[]) {
-        int a = 3, b = 2;
-        var resultado = a + b;
-        System.out.println("resultado suma = " + resultado);
-        
-        resultado = a - b;
-        
-        System.out.println("resultado  resta = " + resultado);
-        
-        resultado = a * b;
-        System.out.println("resultado multiplicacion = " + resultado);
-        
-        var resultado2 = 3D/ b;
-        System.out.println("resultado division = " + resultado2);
-        
-        resultado = a % b;
-        System.out.println("resultado = " + resultado);
-        
-        
-        if(a % 2 ==0)
-            System.out.println("Es número par");
-        else
-            System.out.println("Es número impar");
 
+        int a = 3, b = 2;
+        int c = a + 5 - b;
+
+        System.out.println("c = " + c);
+
+        a += 1; // a  = a +1
+        System.out.println("a = " + a);
+
+        a += 3;
+        System.out.println("a = " + a);
+
+        a -= 2; // a = a - 2;
+        System.out.println("a = " + a);
+        
+        a/=2 ;
+        System.out.println("a = " + a);
+        
+        a *=2;
+        System.out.println("a = " + a);
+        
+        a%=2;
+        System.out.println("a = " + a);
     }
 }
