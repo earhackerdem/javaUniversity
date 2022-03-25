7f4fe66d7d306a00a77ad755bae33c0008246882
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-03-24 19:38:04 -0600

    20 Ejercicio - Tipos Primitivos tipo char

---
+++ src/HolaMundo.java
@@ -0,0 +1,34 @@
+//Mi clase en Java
+
+public class HolaMundo {
+
+    public static void main(String args[]) {
+        char miCaracter = 'ñ';
+        System.out.println("miCaracter = " + miCaracter);
+
+        char varChar = '\u0021';
+        System.out.println("varChar = " + varChar);
+
+        char varCharDecimal = 33;
+        System.out.println("varCharDecimal = " + varCharDecimal);
+
+        char vaCharSImbolo = '!';
+        System.out.println("vaCharSImbolo = " + vaCharSImbolo);
+
+        var varChar1 = '\u0021';
+        System.out.println("varChar1 = " + varChar1);
+
+        var varCharDecimal2 = (char) 33;
+        System.out.println("varCharDecimal2 = " + varCharDecimal2);
+
+        var vaCharSImbolo2 = '!';
+        System.out.println("vaCharSImbolo2 = " + vaCharSImbolo2);
+        
+        int variableEnteraSimbolo = '!';
+        System.out.println("variableEnteraSimbolo = " + variableEnteraSimbolo);
+        
+        int letra = 'A';
+        System.out.println("letra = " + letra);
+
+    }
+}
