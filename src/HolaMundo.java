1a348c5ece5126eccb4a9ca1055c68f42aa07724
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-02 17:42:07 -0600

    36 Ejercicio if else en Java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -11,5 +11,22 @@ public class HolaMundo {
             System.out.println("Condición falsa");
         }
 
+        var numero = 2;
+        var numeroTexto = "Numero desconocido";
+
+        if (numero == 1) {
+            numeroTexto = "Numero uno";
+        } else if (numero == 2) {
+            numeroTexto = "Numero dos";
+        } else if (numero == 3) {
+            numeroTexto = "Numero tres";
+        } else if (numero == 4) {
+            numeroTexto = "Numero cuatro";
+        } else {
+            numeroTexto = "Numero no encontrado";
+        }
+        
+        System.out.println("numero texto = " + numeroTexto);
+
     }
 }
