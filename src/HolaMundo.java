4c22631fe4041d54ebadc07a8a6284771411f0e4
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-02 16:11:04 -0600

    31  Ejercicio - Operador ternario

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,25 +3,12 @@ public class HolaMundo {
 
     public static void main(String args[]) {
 
-        var a = 3;
-        var valorMinimo = 0;
-        var valorMaximo = 10;
+        var resultado = (1 > 2) ? "verdadero" : "falso";
 
-        var resultado = a >= valorMinimo && a <= valorMaximo;
-        if (resultado) {
-            System.out.println("Dentro de rango");
-        }else{
-            System.out.println("Fuera de rango");
-        }
-        
-        var vacaciones = true;
-        var diaDeDescanso = false;
-        
-        if(vacaciones || diaDeDescanso){
-            System.out.println("Padre puede asistir al juego de su hijo");
-        }else{
-            System.out.println("El padre esta ocupado");
-        }
+        System.out.println("resultado = " + resultado);
+
+        var numero = 8;
+        resultado = (numero % 2 == 0) ? "numero par" : "numero impar";
 
     }
 }
