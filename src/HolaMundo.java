44e7e48c12d37c756a884d9fb924b480635e05c5
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-02 13:27:02 -0600

    27 Ejercicio - Operadores Unarios en Java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,27 +3,44 @@ public class HolaMundo {
 
     public static void main(String args[]) {
 
-        int a = 3, b = 2;
-        int c = a + 5 - b;
+        var a = 3;
+        var b = -a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        var c = true;
+        var d = !c;
 
         System.out.println("c = " + c);
+        System.out.println("d = " + d);
 
-        a += 1; // a  = a +1
-        System.out.println("a = " + a);
+        //incremento
+        //1.preincremento (simbolo antes de la variable)
+        var e = 3;
+        var f = ++e; //primero  se incrementa la variable y despues se usa su valor
 
-        a += 3;
-        System.out.println("a = " + a);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
 
-        a -= 2; // a = a - 2;
-        System.out.println("a = " + a);
+        //2. postincremento (simbolo despues de la variable)
+        var g = 5;
+        var h = g++; //primero se utiliza el valor y despues se incrementa
         
-        a/=2 ;
-        System.out.println("a = " + a);
+        System.out.println("g = " + g);// teniamos pendinete un incremento
+        System.out.println("h = " + h);
         
-        a *=2;
-        System.out.println("a = " + a);
+        //decremento
+        //1.predecremento
+        var i = 2;
+        var j = --i;
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
         
-        a%=2;
-        System.out.println("a = " + a);
+        //2.postdecremento
+        var k =4;
+        var l = k--; //primero se usa la variable y queda pendiente el decremento
+        System.out.println("k = " + k); // tenia pendiente un decremento
+        System.out.println("l = " + l);
+
     }
 }
