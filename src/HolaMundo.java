f451e2ab9fa4d1d2bf766e866771bcdf6f578f47
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-02 15:01:58 -0600

    29 Ejercicio - Operadores relacionales

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -8,17 +8,34 @@ public class HolaMundo {
 
         var c = (a == b);
         System.out.println("c = " + c);
-        
-        var d =  a != b;
+
+        var d = a != b;
         System.out.println("d = " + d);
-        
+
         var cadena = "Hola";
         var cadena2 = "Hola";
         var e = cadena == cadena2; //Compara la referencia de objetos
         System.out.println("e = " + e);
-        
+
         var f = cadena.equals(cadena2); //Comparamos contenido de cadenas
         System.out.println("f = " + f);
 
+        var g = a >= b; // mayor que > o el mayor o igual >=
+        System.out.println("g = " + g);
+
+        if (a % 2 == 0) {
+            System.out.println("Es numero par");
+        } else {
+            System.out.println("Es numero impar");
+        }
+
+        var edad = 10;
+        var adulto = 18;
+        if (edad >= adulto) {
+            System.out.println("Es un adulto");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+        
     }
 }
