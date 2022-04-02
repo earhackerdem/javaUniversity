0f2bbe9758c2533fbeee52c1f885f9bbd65ce643
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-02 17:31:22 -0600

    35 Sentencia de control if else

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,20 +3,13 @@ public class HolaMundo {
 
     public static void main(String args[]) {
 
-        var x = 5;
-        var y = 10;
+        var condicion = false;
 
-        var z = ++x + y--;
-
-        System.out.println("x = " + x);
-        System.out.println("y = " + y);
-        System.out.println("z = " + z);
-
-        var resultado = 4 + 5 * 6 / 3;
-        System.out.println("resultado = " + resultado);
-        
-        resultado = (4 + 5) * 6 / 3;
-        System.out.println("resultado = " + resultado);
+        if (condicion) {
+            System.out.println("Condición verdadera");
+        } else {
+            System.out.println("Condición falsa");
+        }
 
     }
 }
