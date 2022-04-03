938651a1bb3d0c29a7461996304f9e4fdbc1363d
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-03 13:23:17 -0500

    47 Atributos y metodos de una clase

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -1,5 +1,13 @@
 package clases;
 
 public class Persona {
-    
+
+    //Atribuos de la clase
+    String nombre;
+    String apellido;
+
+    public void desplegarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido = " + apellido);
+    }
 }
