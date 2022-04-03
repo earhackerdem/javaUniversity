7b09c7a08d4cf0085c923081ed8300eefb94e579
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-03 12:30:03 -0500

    45 Ejercicio - Uso de etiquetas labels

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-
+        
+        inicio:
         for (var contador = 0; contador < 3; contador++) {
             if (contador % 2 != 0) {
-               continue;
+               continue inicio;
             }
              System.out.println("contador = " + contador);
         }
