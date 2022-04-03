20c6a41b7c4c914496e44378e37154775e32d052
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-03 12:20:24 -0500

    43 Ejercicio - Ciclo for

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,20 +4,10 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        /*
-        var contador = 0;
-        while( contador < 3){
+
+        for (var contador = 0; contador < 3; contador++) {
             System.out.println("contador = " + contador);
-            contador ++;
         }
-
-         */
-
-        var contador = 0;
-        do {
-            System.out.println("contador =" + contador);
-            contador++;
-        } while (contador < 0);
 
     }
 }
