e6856bbd24558dd1ad26aeb510ac2b1d6676f436
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-02 18:53:01 -0600

    41 Ejercicio de DoWhile

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,36 +5,11 @@ public class HolaMundo {
 
     public static void main(String args[]) {
 
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Ingrese el mes");
-
-        var mes = Integer.parseInt(teclado.nextLine());
-        var estacion = "Estación desconocida";
-
-        switch (mes) {
-            case 1:
-            case 2:
-            case 12:
-                estacion = "Invierno";
-                break;
-            case 3:
-            case 4:
-            case 5:
-                estacion = "Primavera";
-                break;
-            case 6:
-            case 7:
-            case 8:
-                estacion = "Verano";
-                break;
-            case 9:
-            case 10:
-            case 11:
-                estacion = "Otonio";
-                break;
+        var contador = 0;
+        while( contador < 3){
+            System.out.println("contador = " + contador);
+            contador ++;
         }
-
-        System.out.println("estacion = " + estacion);
-
+        
     }
 }
