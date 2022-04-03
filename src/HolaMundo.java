08a7b6c2be8c0f03685ecdbb75dd6bcdb3ec2c76
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-03 12:08:58 -0500

    42 Ejercicio - Ciclo dowhile

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,12 +4,20 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-
+        /*
         var contador = 0;
         while( contador < 3){
             System.out.println("contador = " + contador);
             contador ++;
         }
-        
+
+         */
+
+        var contador = 0;
+        do {
+            System.out.println("contador =" + contador);
+            contador++;
+        } while (contador < 0);
+
     }
 }
