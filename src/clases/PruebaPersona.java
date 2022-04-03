2a1820214dafa4f47b9b8076f190b682415ca0bf
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-03 13:51:42 -0500

    48 Objetos en Java

---
+++ src/clases/PruebaPersona.java
@@ -0,0 +1,14 @@
+package clases;
+
+public class PruebaPersona {
+
+    public static void main(String[] args) {
+        Persona persona1;
+        persona1 = new Persona();
+        persona1.nombre = "Mike";
+        persona1.apellido = "Perez";
+        persona1.desplegarInformacion();
+   
+    }
+
+}
