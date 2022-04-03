3107b508d1556dc4b72f751d076593fa3b45aa57
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-04-03 12:26:28 -0500

    44 Ejercicio - Palabras Break y continue

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -6,8 +6,17 @@ public class HolaMundo {
     public static void main(String args[]) {
 
         for (var contador = 0; contador < 3; contador++) {
-            System.out.println("contador = " + contador);
+            if (contador % 2 != 0) {
+               continue;
+            }
+             System.out.println("contador = " + contador);
         }
 
+//        for (var contador = 0; contador < 3; contador++) {
+//            if (contador % 2 == 0) {
+//                System.out.println("contador = " + contador);
+//                break;
+//            }
+//        }
     }
 }
